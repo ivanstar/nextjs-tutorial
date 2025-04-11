18bd4a9ffc03b96ab8d068a75ad9408e90205e98
Ivan <ivanstarrr@gmail.com>
2025-04-11 10:26:52 -0700

    2nd commit

--- nextjs-dashboard/app/ui/invoices/table.tsx
+++ nextjs-dashboard/app/ui/invoices/table.tsx
@@ -11,6 +11,8 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
+  console.log('query', query)
+  console.log('currentPage', currentPage)
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
